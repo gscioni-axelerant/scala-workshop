bed9c09db6f60d267df6fbac64c8bf0a0404a83b
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-05 16:42:21 +0200

    template per esempi sul currying

---
+++ src/main/scala/currying.scala
@@ -0,0 +1,4 @@
+package com.axelerant
+
+object currying {}
+//a function that takes multiple arguments can be translated into a series of function calls that each take a single argument.
---
+++ src/test/scala/curryingTest.scala
@@ -0,0 +1,5 @@
+package com.axelerant
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class curryingTest extends AnyFunSuiteLike {}
