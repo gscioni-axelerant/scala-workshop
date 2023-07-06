f8382ff9245c59d9694d6271b8a3b4c8a76da108
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-06 12:11:05 +0200

    esempi sul currying e partially applied functions

--- src/main/scala/currying.scala
+++ src/main/scala/currying.scala
@@ -1,4 +1,16 @@
 package com.axelerant
 
-object currying {}
 //a function that takes multiple arguments can be translated into a series of function calls that each take a single argument.
+object currying {
+
+  def plus(a: Int)(b: Int) = a + b
+  def plus2 = plus(2)(_)
+
+  def wrap(prefix: String)(html: String)(suffix: String) = {
+    prefix + html + suffix
+  }
+  val wrapWithDiv = wrap("<div>")(_: String)("</div>")
+
+
+}
+
--- src/test/scala/curryingTest.scala
+++ src/test/scala/curryingTest.scala
@@ -2,4 +2,17 @@ package com.axelerant
 
 import org.scalatest.funsuite.AnyFunSuiteLike
 
-class curryingTest extends AnyFunSuiteLike {}
+class curryingTest extends AnyFunSuiteLike {
+
+  test("partially applied 1") {
+
+    assert(currying.plus2(3) == 5)
+
+    val content = "contenuto"
+    val result = currying.wrap("<div>")(content)("</div>")
+    assert(result == "<div>contenuto</div>")
+    assert(currying.wrapWithDiv(content) == "<div>contenuto</div>")
+
+  }
+
+}
