06a6110e6146a8f76ac60c24b143c2c92b128a1b
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-12 07:06:10 +0200

    altri esempi sul currying

--- src/main/scala/currying.scala
+++ src/main/scala/currying.scala
@@ -11,6 +11,13 @@ object currying {
   }
   val wrapWithDiv = wrap("<div>")(_: String)("</div>")
 
+  def add1(a:Int, b:Int, c:Int): Int =            a + b + c
+  //add2 è una funzione che prende un parametro a e ritorna una funzione che prende un parametro b e ritorna una funzione che prende un parametro c e ritorna il risultato finale
+  def add2(a:Int): Int => Int => Int = b => c =>  a + b + c
+  //add3 è una funzione definita in modo curried, in questo modo è possibile chiamarla con un sottoinsieme dei parametri, che ritornerà una funzione invece del risultato finale
+  def add3(a:Int)(b:Int)(c:Int): Int =            a + b + c
+
+
 
 }
 
--- src/test/scala/curryingTest.scala
+++ src/test/scala/curryingTest.scala
@@ -4,15 +4,24 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 
 class curryingTest extends AnyFunSuiteLike {
 
-  test("partially applied 1") {
-
+  test("partially applied test") {
     assert(currying.plus2(3) == 5)
+  }
 
+  test("currying 1"){
     val content = "contenuto"
     val result = currying.wrap("<div>")(content)("</div>")
     assert(result == "<div>contenuto</div>")
     assert(currying.wrapWithDiv(content) == "<div>contenuto</div>")
+  }
 
+  test("currying 2"){
+    val v1 = currying.add1(1, 2, 3)
+    val v2 = currying.add2(1)(2)(3)
+    val v3 = currying.add3(1)(2)(3)
+
+    assert(v1==v2)
+    assert(v2==v3)
   }
 
 }
