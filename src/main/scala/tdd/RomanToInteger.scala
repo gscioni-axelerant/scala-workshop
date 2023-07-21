305a2543a5f41dfe83f7d691271a2f4947fbf390
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-21 15:47:41 +0200

    soluzione ricorsiva tail recursive + tests di integrazione

--- src/main/scala/tdd/RomanToInteger.scala
+++ src/main/scala/tdd/RomanToInteger.scala
@@ -1,28 +1,33 @@
 package com.axelerant
 package tdd
 
+import scala.annotation.tailrec
+
 object RomanToInteger {
 
   def fromArabic(arabic: Int): String =
     val regole = Seq(
+      1000 -> "M",
+      900 -> "CM",
+      500 -> "D",
+      400 -> "CD",
+      100 -> "C",
+      90 -> "XC",
+      50 -> "L",
+      40 -> "XL",
       10 -> "X",
+      9 -> "IX",
       5 -> "V",
-      1 -> "I"
-    )
-//    var resto = arabic
-//    regole.map {
-//      case (arab, roman) =>
-//        val volte = resto / arab
-//        resto%=arab
-//        roman*volte
-//    }.mkString
+      4 -> "IV",
+      1 -> "I")
 
-    def toRoman(num: Int, seq: Seq[(Int, String)]): String = seq match
-      case Nil => ""
+    @tailrec
+    def toRoman(acc: String, num: Int, seq: Seq[(Int, String)]): String = seq match
+      case Nil => acc
       case (arab, roman) :: tail =>
         val volte = num / arab
         val resto = num % arab
-        roman * volte + toRoman(resto, tail)
+        toRoman(acc + (roman * volte), resto, tail)
 
-    toRoman(arabic, regole)
+    toRoman("", arabic, regole)
 }
--- src/test/scala/tdd/RomanToIntegerTest.scala
+++ src/test/scala/tdd/RomanToIntegerTest.scala
@@ -11,12 +11,26 @@ class RomanToIntegerTest extends AnyFunSuiteLike {
     1 -> "I",
     2 -> "II",
     3 -> "III",
-    //pensare alla sottrazione delle I
+    4 -> "IV",
     5 -> "V",
     6 -> "VI",
+    9 -> "IX",
     10 -> "X",
     15 -> "XV",
-    20 -> "XX"
+    20 -> "XX",
+    40 -> "XL",
+    50 -> "L",
+    90 -> "XC",
+    100 -> "C",
+    400 -> "CD",
+    500 -> "D",
+    900 -> "CM",
+    1000 -> "M",
+
+    //integration tests
+    3497 -> "MMMCDXCVII",
+    3433 -> "MMMCDXXXIII",
+    4999 -> "MMMMCMXCIX"
   )
 
   Examples.foreach((arab: Int, roman: String) => {
@@ -34,6 +48,9 @@ class RomanToIntegerTest extends AnyFunSuiteLike {
     println(times)
     reminder %= roman
     println(reminder)
+
+
+    println(fromArabic(3433))
 
   }
 }
