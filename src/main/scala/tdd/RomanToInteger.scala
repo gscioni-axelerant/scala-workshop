eeba6ddfa84268cffc48564d40fc634cf68e87bd
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-21 14:51:39 +0200

    ulteriori step esercizio

--- src/main/scala/tdd/RomanToInteger.scala
+++ src/main/scala/tdd/RomanToInteger.scala
@@ -3,6 +3,17 @@ package tdd
 
 object RomanToInteger {
 
-  def fromArabic(arabic: Int) : String = "I"
-
+  def fromArabic(arabic: Int): String =
+    val regole = Seq(
+      10 -> "X",
+      5 -> "V",
+      1 -> "I"
+    )
+    var resto = arabic
+    regole.map {
+      case (arab, roman) =>
+        val volte = resto / arab
+        resto%=arab
+        roman*volte
+    }.mkString
 }
--- src/test/scala/tdd/RomanToIntegerTest.scala
+++ src/test/scala/tdd/RomanToIntegerTest.scala
@@ -1,13 +1,39 @@
 package com.axelerant
 package tdd
 
-import com.axelerant.tdd.RomanToInteger.fromArabic
+import tdd.RomanToInteger.fromArabic
+
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class RomanToIntegerTest extends AnyFunSuiteLike {
 
-  test("converte 1 in I") {
-    assert(fromArabic(1) == "I")
-  }
+  val Examples = Seq(
+    1 -> "I",
+    2 -> "II",
+    3 -> "III",
+    //pensare alla sottrazione delle I
+    5 -> "V",
+    6 -> "VI",
+    10 -> "X",
+    15 -> "XV",
+    20 -> "XX"
+  )
 
+  Examples.foreach((arab: Int, roman: String) => {
+    test(s"converte $arab in $roman") {
+      assert(fromArabic(arab) == roman)
+    }
+  })
+
+  test(""){
+    val num = 15
+    val roman = 10
+    var reminder = num
+
+    val times = num / roman
+    println(times)
+    reminder %= roman
+    println(reminder)
+
+  }
 }
