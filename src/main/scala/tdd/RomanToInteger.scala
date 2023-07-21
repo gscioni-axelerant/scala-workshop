95940a4562a7970be9d53b09001f97683e851a62
Roberto Gasdia <roberto.gasdia@innoteam.it>
2023-07-21 15:18:09 +0200

    soluzione ricorsiva non tail recursive

--- src/main/scala/tdd/RomanToInteger.scala
+++ src/main/scala/tdd/RomanToInteger.scala
@@ -9,11 +9,20 @@ object RomanToInteger {
       5 -> "V",
       1 -> "I"
     )
-    var resto = arabic
-    regole.map {
-      case (arab, roman) =>
-        val volte = resto / arab
-        resto%=arab
-        roman*volte
-    }.mkString
+//    var resto = arabic
+//    regole.map {
+//      case (arab, roman) =>
+//        val volte = resto / arab
+//        resto%=arab
+//        roman*volte
+//    }.mkString
+
+    def toRoman(num: Int, seq: Seq[(Int, String)]): String = seq match
+      case Nil => ""
+      case (arab, roman) :: tail =>
+        val volte = num / arab
+        val resto = num % arab
+        roman * volte + toRoman(resto, tail)
+
+    toRoman(arabic, regole)
 }
